809a858d0d1d00e41f41891f038e77527ca80ae7
Gabriele Nicola Saragò <gabriele.sarago@hotmail.com>
2016-01-04 19:54:44 +0100

    Aggiunta la verifica della risoluzione dello schermo.

--- Gioco/src/gioco/Launcher.java
+++ Gioco/src/gioco/Launcher.java
@@ -1,9 +1,20 @@
 package gioco;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JOptionPane;
+
 public class Launcher {
 	
 	public static void main (String[]args){
 		Gioco gioco = new Gioco("Labirinto!",1200, 700);
-		gioco.start();
+		Dimension schermo = Toolkit.getDefaultToolkit().getScreenSize();
+		int larghezza = (int) schermo.getWidth();
+		int altezza = (int) schermo.getHeight();
+		if(larghezza < 1200 || altezza < 700)
+			JOptionPane.showMessageDialog(null, "La risoluzione dello schermo non supporta i requisiti minimi.");
+		else
+			gioco.start();
 	}
 }
